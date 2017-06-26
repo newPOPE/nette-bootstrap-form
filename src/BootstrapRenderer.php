79110d02c7a0777ffef2b2fb6dfda928fd650ab9
Adam Stipak <adam.stipak@gmail.com>
2017-06-26 10:47:30 +0200

    Update default rendered to 2.4.

--- src/BootstrapRenderer.php
+++ src/BootstrapRenderer.php
@@ -3,105 +3,113 @@
 namespace Tomaj\Form\Renderer;
 
 use Nette;
-use Nette\Forms\Rendering\DefaultFormRenderer;
 use Nette\Forms\Controls;
+use Nette\Forms\Rendering\DefaultFormRenderer;
 use Nette\Utils\Html;
 
-class BootstrapRenderer extends DefaultFormRenderer
-{
-    public $wrappers = array(
-        'form' => array(
-            'container' => null,
-        ),
-        'error' => array(
-            'container' => 'div class="alert alert-danger"',
-            'item' => 'p',
-        ),
-        'group' => array(
-            'container' => 'fieldset',
-            'label' => 'legend',
-            'description' => 'p',
-        ),
-        'controls' => array(
-            'container' => 'div',
-        ),
-        'pair' => array(
-            'container' => 'div class=form-group',
-            '.required' => 'required',
-            '.optional' => null,
-            '.odd' => null,
-            '.error' => 'has-error',
-        ),
-        'control' => array(
-            'container' => 'div class=col-sm-9',
-            '.odd' => null,
-            'description' => 'span class=help-block',
-            'requiredsuffix' => '',
-            'errorcontainer' => 'span class=help-block',
-            'erroritem' => '',
-            '.required' => 'required',
-            '.text' => 'text form-control',
-            '.password' => 'text form-control',
-            '.select' => 'text form-control',
-            '.file' => 'text',
-            '.submit' => 'button',
-            '.image' => 'imagebutton',
-            '.button' => 'button',
-        ),
-        'label' => array(
-            'container' => 'div class="col-sm-3 control-label"',
-            'suffix' => null,
-            'requiredsuffix' => '',
-        ),
-        'hidden' => array(
-            'container' => 'div',
-        ),
-    );
+class BootstrapRenderer extends DefaultFormRenderer {
 
-    /**
-     * Provides complete form rendering.
-     * @param  Nette\Forms\Form
-     * @param  string 'begin', 'errors', 'ownerrors', 'body', 'end' or empty to render all
-     * @return string
-     */
-    public function render(Nette\Forms\Form $form, $mode = null)
-    {
-        $form->getElementPrototype()->addClass('form-horizontal');
-        $form->getElementPrototype()->setNovalidate('novalidate');
-        foreach ($form->getControls() as $control) {
-            if ($control instanceof Controls\Button) {
-                if (strpos($control->getControlPrototype()->getClass(), 'btn') === FALSE) {
-                    $control->getControlPrototype()->addClass(empty($usedPrimary) ? 'btn btn-primary' : 'btn btn-default');
-                    $usedPrimary = true;
-                }
-            } elseif ($control instanceof Controls\TextBase ||
-                $control instanceof Controls\SelectBox ||
-                $control instanceof Controls\MultiSelectBox) {
-                $control->getControlPrototype()->addClass('form-control');
-            } elseif ($control instanceof Controls\Checkbox ||
-                $control instanceof Controls\CheckboxList ||
-                $control instanceof Controls\RadioList) {
-                $control->getSeparatorPrototype()->setName('div')->addClass($control->getControlPrototype()->type);
-            }
-        }
+  public $wrappers = [
+    'form'     => [
+      'container' => null,
+    ],
+    'error'    => [
+      'container' => 'div class="alert alert-danger"',
+      'item'      => 'p',
+    ],
+    'group'    => [
+      'container'   => 'fieldset',
+      'label'       => 'legend',
+      'description' => 'p',
+    ],
+    'controls' => [
+      'container' => 'div',
+    ],
+    'pair'     => [
+      'container' => 'div class=form-group',
+      '.required' => 'required',
+      '.optional' => null,
+      '.odd'      => null,
+      '.error'    => 'has-error',
+    ],
+    'control'  => [
+      'container'      => 'div class=col-sm-9',
+      '.odd'           => null,
+      'description'    => 'span class=help-block',
+      'requiredsuffix' => '',
+      'errorcontainer' => 'span class=help-block',
+      'erroritem'      => '',
+      '.required'      => 'required',
+      '.text'          => 'text form-control',
+      '.email'         => 'text form-control',
+      '.password'      => 'text form-control',
+      '.select'        => 'text form-control',
+      '.file'          => 'text',
+      '.submit'        => 'button',
+      '.image'         => 'imagebutton',
+      '.button'        => 'button',
+    ],
+    'label'    => [
+      'container'      => 'div class="col-sm-3 control-label"',
+      'suffix'         => null,
+      'requiredsuffix' => '',
+    ],
+    'hidden'   => [
+      'container' => 'div',
+    ],
+  ];
 
-        return parent::render($form, $mode);
+  /**
+   * Provides complete form rendering.
+   * @param  Nette\Forms\Form
+   * @param  string 'begin', 'errors', 'ownerrors', 'body', 'end' or empty to render all
+   * @return string
+   */
+  public function render (Nette\Forms\Form $form, $mode = null) {
+    if ($this->form !== $form) {
+      $this->form = $form;
     }
 
-  public function renderControl(Nette\Forms\IControl $control)
-  {
+    $form->getElementPrototype()->addClass('form-horizontal');
+    $form->getElementPrototype()->setNovalidate('novalidate');
+    foreach ($form->getControls() as $control) {
+      if ($control instanceof Controls\Button) {
+        if (strpos($control->getControlPrototype()->getClass(), 'btn') === false) {
+          $control->getControlPrototype()->addClass(empty($usedPrimary) ? 'btn btn-primary' : 'btn btn-default');
+          $usedPrimary = true;
+        }
+      } elseif ($control instanceof Controls\TextBase ||
+        $control instanceof Controls\SelectBox ||
+        $control instanceof Controls\MultiSelectBox
+      ) {
+        $control->getControlPrototype()->addClass('form-control');
+      } elseif ($control instanceof Controls\Checkbox ||
+        $control instanceof Controls\CheckboxList ||
+        $control instanceof Controls\RadioList
+      ) {
+        $control->getSeparatorPrototype()->setName('div')->addClass($control->getControlPrototype()->type);
+      }
+    }
+
+    return parent::render($form, $mode);
+  }
+
+  public function renderControl (Nette\Forms\IControl $control) {
+    $form = $control->getForm();
+    if ($this->form !== $form) {
+      $this->form = $form;
+    }
+
     $body = $this->getWrapper('control container');
     if ($this->counter % 2) {
-      $body->class($this->getValue('control .odd'), TRUE);
+      $body->class($this->getValue('control .odd'), true);
     }
 
     $description = $control->getOption('description');
     if ($description instanceof Html) {
       $description = ' ' . $description;
-
     } elseif (is_string($description)) {
       $description = ' ' . $this->getWrapper('control description')->setText($control->translate($description));
-
     } else {
       $description = '';
     }
@@ -110,14 +118,15 @@ class BootstrapRenderer extends DefaultFormRenderer
       $description = $this->getValue('control requiredsuffix') . $description;
     }
 
-    $control->setOption('rendered', TRUE);
+    $control->setOption('rendered', true);
     $el = $control->getControl();
     if ($el instanceof Html && $el->getName() === 'input') {
-      $el->class($this->getValue("control .$el->type"), TRUE);
+      $el->class($this->getValue("control .$el->type"), true);
     }
-    if($control instanceof Controls\SelectBox) {
-      $el->class($this->getValue("control .select"), TRUE);
+    if ($control instanceof Controls\SelectBox) {
+      $el->class($this->getValue("control .select"), true);
     }
+
     return $body->setHtml($el . $description . $this->renderErrors($control));
   }
 }
